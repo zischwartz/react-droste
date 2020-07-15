b72de7927a2bf0737aed88fbeb4cabf5a322e58e
Zach Schwartz <zischwartz@gmail.com>
2020-07-15 13:51:14 -0400

    Updates

---
+++ precache-manifest.999e9dc3b726862668a8804a0445d51a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5e2ba38e4b348bf877d00c8760041f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b65772bb3c11383c37c",
+    "url": "./static/css/main.c972a840.chunk.css"
+  },
+  {
+    "revision": "67a246cc17c253d25eff",
+    "url": "./static/js/2.e22d5c42.chunk.js"
+  },
+  {
+    "revision": "462f991c1d2ef16102b314f225132ac4",
+    "url": "./static/js/2.e22d5c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b65772bb3c11383c37c",
+    "url": "./static/js/main.a278f39b.chunk.js"
+  },
+  {
+    "revision": "433db8ff0214b606faf8",
+    "url": "./static/js/runtime-main.9e193f9e.js"
+  }
+]);
